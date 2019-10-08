f64d61a0cda4a883181f97b8ce717d88a90a4d58
OswinOswald <linaa_2017@bk.ru>
2019-10-08 15:54:04 +0300

    first app

--- core/Oswin/views.py
+++ core/Oswin/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from pathlib import Path
+from django.http import HttpResponse
+
+
 def index(_):
     p = Path(__file__).parent / "index.html"
     with p.open("r", encoding = 'utf-8') as f:
--- core/core/settings.py
+++ core/core/settings.py
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ('C:/mod/TeachMeSkills/OswinOswald/oswinos/core/static',
+)
