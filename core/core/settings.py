c41b2772014388d7c3bce56200dcfcb71206294a
OswinOswald <linaa_2017@bk.ru>
2019-10-09 14:50:09 +0300

    222

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,10 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = ('C:/mod/TeachMeSkills/OswinOswald/oswinos/core/Oswin/static',
-# )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'Oswin', 'static')
+STATICFILES_DIRS = ('C:/mod/TeachMeSkills/OswinOswald/oswinos/core/Oswin/static',
+)
